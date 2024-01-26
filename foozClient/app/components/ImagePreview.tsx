32421c3ad46809bfc51bfce2b1f8757245b33536
ronhaland <ronnyhaland@gmail.com>
2024-01-26 10:28:41 +0100

    fix(infra): fix build run condition

--- foozClient/app/components/ImagePreview.tsx
+++ foozClient/app/components/ImagePreview.tsx
@@ -2,7 +2,7 @@ export const ImagePreview = () => {
   const className =
     "w-[33%] [clip-path:polygon(0_0,100%_0,100%_100%,20%_100%)]";
   return (
-    <div className="relative flex flex-row border-4 lg:border-[1rem] border-slate-950 lg:rounded-lg m-8">
+    <div className="relative flex flex-row border-4 lg:border-[1rem] border-slate-950 bg-slate-950 lg:rounded-lg m-8">
       <img src="images/Fooz1.jpg" alt="" className="relative w-[33%]" />
       <div className={`${className} ml-[-6%] bg-slate-950`}></div>
       <img
